31d33ab10c07689dfe24e4cd9498f4955a2573d9
Zakir Syed <35612166+zakir8251@users.noreply.github.com>
2018-12-05 17:38:15 -0800

    Create natural_order_sort.cpp
    usage of lambda, custom comparator, string parsing

---
+++ natural_order_sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+print_container(vector<string>& V)
+{
+	for(auto& v:V)
+		cout<<v<<" ";
+	cout<<endl;
+}
+
+int parse_digit(string& str)
+{
+	int start=str.find('_')+1;
+	int end = str.find('.');
+	return stoi(str.substr(start, end-start+1), nullptr);
+}
+
+int my_sort_fn(string& s1, string& s2)
+{
+	return parse_digit(s1) < parse_digit(s2);
+}
+
+/*
+  Sort the list of filenames in natural order:-
+  Input Sequence       :RGI_0.pdf RGI_1.pdf RGI_2.pdf RGI_10.pdf RGI_8.pdf RGI_50.pdf 
+  Lexicographical order:RGI_0.pdf RGI_1.pdf RGI_10.pdf RGI_2.pdf RGI_50.pdf RGI_8.pdf
+  Natural order        :RGI_0.pdf RGI_1.pdf RGI_2.pdf RGI_8.pdf RGI_10.pdf RGI_50.pdf
+*/
+
+int main()
+{
+	vector<string> V={"RGI_0.pdf", "RGI_1.pdf", "RGI_2.pdf", "RGI_10.pdf", "RGI_8.pdf", "RGI_50.pdf" };
+	print_container(V); //RGI_0.pdf RGI_1.pdf RGI_2.pdf RGI_10.pdf RGI_8.pdf RGI_50.pdf 
+  sort(V.begin(), V.end(), my_sort_fn);
+	print_container(V); //RGI_0.pdf RGI_1.pdf RGI_2.pdf RGI_8.pdf RGI_10.pdf RGI_50.pdf 
+  //Solution using labda []
+	sort(V.begin(), V.end(), [] (string s1, string s2){return parse_digit(s1) < parse_digit(s2);});
+	print_container(V); //RGI_0.pdf RGI_1.pdf RGI_2.pdf RGI_8.pdf RGI_10.pdf RGI_50.pdf 
+
+ 	return 0;
+}
